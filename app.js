1000dae15bc2c867048fef0f84e0f5c066a6e026
Akash <akashstunes@gmail.com>
2022-01-13 14:26:34 +0530

    added nav menu function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let hamburger = document.querySelector('.hamburger');
 let navOpen = document.querySelector('.nav');
+let logo = document.querySelector('.h-logo');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('close');
     navOpen.classList.toggle('open');
+    logo.classList.toggle('close');
 })
